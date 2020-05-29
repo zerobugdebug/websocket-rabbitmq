23d0b6134b5508318cac04f99f4de3589727a971
digitalcio <digitalcio.github@go.intexid.com>
2020-05-29 16:35:33 -0400

    Changed maxMessageSize

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ const (
 	writeWait = 10 * time.Second
 
 	// Maximum message size allowed from peer.
-	maxMessageSize = 8192
+	maxMessageSize = 4096
 
 	// Time allowed to read the next pong message from the peer.
 	pongWait = 10 * time.Second
